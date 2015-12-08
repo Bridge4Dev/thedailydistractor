ce8d792eb9ca0857daa233e4a484c9e19e39af47
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-12-08 13:37:27 -0500

    Create home page with place holder text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+	# Return a full title on each page
+	def full_title(page_title = "")
+		base_title = "The Daily Distractor"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
+	
 end
