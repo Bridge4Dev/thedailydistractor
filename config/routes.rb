6ebd1debef53c4c13b8ea25a736ba582609958a5
Matthew Byrne <mattbyrne@Matthews-MacBook-Pro.local>
2015-12-10 11:49:52 -0500

    Create Users Controller with strong params and methods

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#home'
+  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
